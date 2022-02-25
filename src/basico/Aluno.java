67e702eaf521559a22c9f3431865ffd99480e2f4
artaxerxes001 <artaxerxesnazare@gmail.com>
2022-02-25 19:30:14 +0100

    exceptions personalizadas

--- src/basico/Aluno.java
+++ src/basico/Aluno.java
@@ -2,8 +2,10 @@ package basico;
 
 public class Aluno {
     public final String nome;
+    public final double nota;
 
-    public Aluno(String nome) {
+    public Aluno(String nome, double nota) {
         this.nome = nome;
+        this.nota = nota;
     }
 }
---
+++ src/personalizada/TesteValidacoes.java
@@ -0,0 +1,19 @@
+package personalizada;
+
+import basico.Aluno;
+
+public class TesteValidacoes {
+    public static void main(String[] args) {
+        try {
+            Aluno aluno = new Aluno("Ana", -1);
+            Validar.aluno(aluno);
+
+        } catch (StringVaziaException e) {
+            System.out.println(e.getMessage());
+        } catch (NumeroForaIntervaloException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Fim :)");
+    }
+
+}
